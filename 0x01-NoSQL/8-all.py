8586acf595033c88685fdf48b26c7f23e84a40a9
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-07-18 11:56:05 -0700

    for 0 to 8

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" list all """
+
+def list_all(c):
+    """ lists all documents """
+    if __name__ != "__main__":
+        if list(c.find()) and len(list(c.find())):
+            return list(c.find())
+        else:
+            return []
