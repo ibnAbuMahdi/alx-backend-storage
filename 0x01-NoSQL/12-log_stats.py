d88dc485db497f9ae5cce167185885d9d3bb0fea
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-07-21 09:49:18 -0700

    crxn 9 for 12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,23 +3,23 @@
 from pymongo import MongoClient
 
 if __name__ != '__main__':
-    client = MongoClient('mongodb://127.0.0.1:27017')
+    client = MongoClient("mongodb://127.0.0.1:27017")
     col = client.logs.nginx
     no = col.count_documents()
 
-    get = col.count_documents({'method': 'GET'})
-    post = col.count_documents({'method': 'POST'})
-    put = col.count_documents({'method': 'PUT'})
-    patch = col.count_documents({'method': 'PATCH'})
-    delete = col.count_documents({'method': 'DELETE'})
+    get = col.count_documents({"method": "GET"})
+    post = col.count_documents({"method": "POST"})
+    put = col.count_documents({"method": "PUT"})
+    patch = col.count_documents({"method": "PATCH"})
+    delete = col.count_documents({"method": "DELETE"})
 
-    get_path = col.count_documents({'method': 'GET', 'path': '/status'})
+    get_path = col.count_documents({"path": "/status"})
 
-    print(str(no) + ' logs')
-    print('Methods:')
-    print('\t method GET: ' + str(get))
-    print('\t method POST: ' + str(post))
-    print('\t method PUT: ' + str(put))
-    print('\t method PATCH: ' + str(patch))
-    print('\t method DELETE: ' + str(delete))
-    print(str(get_path) + ' status check')
+    print(str(no) + " logs")
+    print("Methods:")
+    print("\t method GET: " + str(get))
+    print("\t method POST: " + str(post))
+    print("\t method PUT: " + str(put))
+    print("\t method PATCH: " + str(patch))
+    print("\t method DELETE: " + str(delete))
+    print(str(get_path) + " status check")
