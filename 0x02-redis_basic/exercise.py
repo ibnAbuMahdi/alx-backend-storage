6eccbf9e83f6661453d84c71a0a9d4f9a6b22761
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-07-21 13:52:54 -0700

    for 1 to 4

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,15 +1,74 @@
-#!/usr/bin/env python3
-""" exercise """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        inputs_key = method.__qualname__ + ':inputs'
+        outputs_key = method.__qualname__ + ':outputs'
+
+        self._redis.rpush(inputs_key, str(args))
+
+        output = method(self, *args, **kwargs)
+
+        self._redis.rpush(outputs_key, str(output))
+
+        return output
+    return wrapper
+
 
 class Cache:
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: Callable = None) -> Union[str, bytes, int, float, None]:
+        if not self._redis.exists(key):
+            return None
+
+        data = self._redis.get(key)
+        if fn is None:
+            return data
+
+        return fn(data)
+
+    def get_str(self, key: str) -> Union[str, None]:
+        return self.get(key, lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> Union[int, None]:
+        return self.get(key, int)
+
+
+def replay(method: Callable):
+    red = redis.Redis.from_url('redis://localhost:6379/0')
+    inputs_key = method.__qualname__ + ':inputs'
+    outputs_key = method.__qualname__ + ':outputs'
+
+    call_count = red.llen(inputs_key)
+
+    print(f"{method.__qualname__} was called {call_count} times:")
+
+    for i in range(call_count):
+        inputs = red.lindex(inputs_key, i).decode()
+        output = red.lindex(outputs_key, i).decode()
+        print(f"{method.__qualname__}(*{eval(inputs)}) -> {output}")
---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+""" web """
+import requests
+import redis
+import functools
+
+redis_conn = redis.Redis()
+
+
+def use_cache(expiration_time):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(url):
+            content = redis_conn.get(url)
+            if content is not None:
+                return content.decode('utf-8')
+
+            content = func(url)
+            redis_conn.setex(url, expiration_time, content)
+
+            return content
+
+        return wrapper
+    return decorator
+
+
+def count_accesses(func):
+    @functools.wraps(func)
+    def wrapper(url):
+        key = f"count:{url}"
+        redis_conn.incr(key)
+        return func(url)
+
+    return wrapper
+
+
+@use_cache(expiration_time=10)
+@count_accesses
+def get_page(url: str) -> str:
+    response = requests.get(url)
+    return response.text
