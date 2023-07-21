fa00d9da47f53f69f6278b36b5111075488d8c38
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-07-21 10:38:21 -0700

    for 102

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+""" log doc parsing """
+from pymongo import MongoClient
+
+if __name__ == '__main__':
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    col = client.logs.nginx
+    no = col.count_documents({})
+
+    get = col.count_documents({"method": "GET"})
+    post = col.count_documents({"method": "POST"})
+    put = col.count_documents({"method": "PUT"})
+    patch = col.count_documents({"method": "PATCH"})
+    delete = col.count_documents({"method": "DELETE"})
+
+    get_path = col.count_documents({"path": "/status"})
+
+    print(str(no) + " logs")
+    print("Methods:")
+    print("\tmethod GET: " + str(get))
+    print("\tmethod POST: " + str(post))
+    print("\tmethod PUT: " + str(put))
+    print("\tmethod PATCH: " + str(patch))
+    print("\tmethod DELETE: " + str(delete))
+    print(str(get_path) + " status check")
+    
+
+    pipeline = [
+        {
+            "$group": {
+                "_id": "$ip",
+                "frequency": {"$sum": 1}
+            }
+        },
+        {
+            "$sort": {"frequency": -1}
+        }
+    ]
+
+    result = col.aggregate(pipeline)
+    print("IPs:")
+    for doc in list(result)[:10]:
+        print(f"\t{doc['_id']}: {doc['frequency']}")
